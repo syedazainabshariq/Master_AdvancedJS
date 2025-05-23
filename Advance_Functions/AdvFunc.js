d13b7e57df73f1ff083039aad5faa890b06595c3
zainab <shariq_ali@aol.co.uk>
2025-05-23 11:12:28 +0500

    Add files via upload
    This repository emphasizes the main things in Advance Functions when learning Advanced JS, This way not only is the lesson being learned, but also React, and NextJS becomes easier, since it's all about functionality which is in Javascript, and to do Advanced work it is crucial to go step by step, like how first JavaScript was being used, but now it all relies on Advanced JS

--- Advance_Functions/AdvFunc.js
+++ Advance_Functions/AdvFunc.js
@@ -1,1 +1,55 @@
+// Advance Function in JavaScript:
 
+// Closures
+
+function outer() {
+  let count = 0; // private variable
+  return function inner() {
+    count++;
+    return count;
+  };
+}
+const counter = outer();
+console.log(counter()); // 1
+console.log(counter()); // 2
+
+
+
+
+// Currying
+
+function add(a){
+    return function (b){
+        return a + b;
+    };
+}
+const add5 = add(5);
+console.log(add(3)); // OUTPUT: 8
+
+
+
+// Partial Application
+
+function multiply(a, b) {
+    return a*b;
+}
+const double = multiply.bind(null, 2);
+console.log(double(4));
+
+
+// IIFE (Immediately Invoked Functions Expression)
+
+(function () {
+    console.log("This runs immediately!")
+})(); 
+
+
+// Fuyction Composition
+
+const add2 = (x) => x + 2;
+const multiply3 = (x) => x*3;
+
+const compose = (f, g) => (x) => f(g(x));
+const add2ThenMultiply3 = compose(multiply3, add2);
+
+console.log(add2ThenMultiply3(4))
